316f95f8bd705cf611d06c41cb050c366dead2e9
Timothy Head <head.timothy3@googlemail.com>
2019-11-19 10:05:41 +0000

    did finish project

--- Facebook test/AppDelegate.swift
+++ Facebook test/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        
+        let handled = ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        Settings.appID = "***************"
+        return handled
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,6 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        ApplicationDelegate.shared.application(app, open: url, options: options)
     }
 
 
--- Facebook test/ViewController.swift
+++ Facebook test/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        if error != nil {
+            print(error!)
+        } else if result!.isCancelled {
+            print("user has canceled login")
+        } else {
+            if (result?.grantedPermissions.contains("email"))! {
+                 let graphResult = GraphRequest(graphPath: "me", parameters: ["fields": "email,name"])
+                graphResult.start { (connection, result, error) in
+                    if error != nil {
+                        print(error!)
+                    } else {
+                        if let userDeets = result {
+                            print(userDeets)
+                        }
+                    }
+                }
+            }
+        }
+            
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("logout")
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        loginButton.permissions = ["public_profile", "email"]
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
+        Settings.appID =  "404459930463566"
+        
     }
 
 
